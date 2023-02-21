d65dc0820434b47dd2b021edab3d3e0148bf94e8
Morella92 <morena.piem@outlook.com>
2023-02-21 16:42:00 +0100

    FEAT milestone 2 completed

--- js/app.js
+++ js/app.js
@@ -3,9 +3,7 @@
 // text, una stringa che indica il testo del todo
 // done, un booleano (true/false) che indica se il todo è stato fatto oppure no
 
-// MILESTONE 2
-// Visualizzare a fianco ad ogni item ha una “x”: cliccando su di essa, 
-// il todo viene rimosso dalla lista.
+
 // MILESTONE 3
 // Predisporre un campo di input testuale e un pulsante “aggiungi”: 
 // cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo,
@@ -41,21 +39,18 @@ createApp({
     data() {
         return {
 
-            // MILESTONE 1
-            // Stampare all’interno di una lista HTML un item per ogni todo. 
-            // Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
             listItem: list
         }
     },
 
     methods: {
 
-        isTrue: function(){
+        //MILESTONE 2
+        //Visualizzare a fianco ad ogni item ha una “x”: cliccando su di essa,
+        //il todo viene rimosso dalla lista. 
+        removeItem: function (itemIndex) {
 
-            // if(this.done === true){
-
-            //     this.text =
-            // }
+            this.listItem.splice(itemIndex, 1)
         }
     }
 }).mount('#app')
